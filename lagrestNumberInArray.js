a33fcbd43188ca46e6018b67d895384f26a7aff4
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-10-28 18:48:49 +0800

    find lagest number in nested array

---
+++ lagrestNumberInArray.js
@@ -0,0 +1,24 @@
+// Return Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+//NOTE my solution
+function largestOfFour(arr) {
+  let largestNumberInIndexArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(i);
+    const sortedNestedArr = arr[i].sort((a, b) => a - b);
+    // console.log(sortedNestedArr.slice(-1));
+    largestNumberInIndexArray.push(sortedNestedArr.slice(-1));
+  }
+  const result = largestNumberInIndexArray.flat();
+  return result;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
